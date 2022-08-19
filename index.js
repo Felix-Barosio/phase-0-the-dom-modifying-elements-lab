b2f6066dc1084a34c31258337ebc3863b810d68f
Felix <barosiofelix@gmail.com>
2022-08-19 08:54:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const mains = document.getElementById('main');
+mains.remove();
+
+// const element = document.querySelector('body');
+// element.removeChild(document.querySelector('#main'));
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+// newHeader.innerHTML = 'Felix is the champion'
+newHeader.textContent = 'Felix is the champion';
+
+document.body.append(newHeader)
